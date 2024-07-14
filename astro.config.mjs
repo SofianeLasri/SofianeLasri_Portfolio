b5ee6ec544067ef9fe1c74af16c97904b873dcd5
SofianeLasri <alasri250@gmail.com>
2024-07-14 15:39:25 +0200

    Création de la collection des projets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    i18n: {
-        defaultLocale: "fr",
-        locales: ["fr", "en"],
-    },
-    adapter: vercel()
+  output: 'server',
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["fr", "en"]
+  },
+  adapter: vercel(),
+  integrations: [mdx()]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -18,6 +18,18 @@ const softSkillsCollection = defineCollection({
     })
 });
 
+const projectsCollection = defineCollection({
+    schema: ({image}) => z.object({
+        category: z.string(),
+        name: z.string(),
+        description: z.string(),
+        cover: image().refine((img) => img.width >= 1080, {
+            message: "Cover image must be at least 1080 pixels wide!",
+        }),
+    }),
+});
+
 export const collections = {
-    'homepage': softSkillsCollection
+    'homepage': softSkillsCollection,
+    'projects': projectsCollection,
 };
